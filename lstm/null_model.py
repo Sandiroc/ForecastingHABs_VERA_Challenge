96c81132e089ef7edf66676c26a925fb1e6964c7
Sandiroc <bsandilya1908@gmail.com>
2024-04-15 12:10:20 -0400

    split into reservoirs

--- lstm/null_model.py
+++ lstm/null_model.py
@@ -1,15 +1,16 @@
 import numpy as np
+import sys
 import keras
 from keras.layers import Dense, Activation, Dropout, LSTM
 from keras.models import Sequential, load_model
 
-
+"""
 class HABForecastModel():
-    """Deep learning architecture with LSTM layer"""
+    # Deep learning architecture with LSTM layer
 
     
     def __init__(self, forecast_dur: int):
-        """Start a sequential model that will allow us to customize layers"""
+        # Start a sequential model that will allow us to customize layers
         self.model = Sequential()
         self.forecast_horizon = forecast_dur
-
+"""
--- scripts/format_data.py
+++ scripts/format_data.py
@@ -2,12 +2,15 @@ from datetime import datetime
 import pandas as pd
 import read_data
 
-# get the data
-read_data.read()
-
-def format(model_type):
+def format(model_type, reservoir):
     """Format the data obtained from the VERA website, parameter included depending on what features are included in the lstm"""
 
+    if (model_type != "null"):
+        raise Exception("Invalid model type input!")
+
+    # get the data
+    read_data.read()
+    
     # get date
     date = datetime.now()
     datestring = date.strftime("%Y-%m-%d")
@@ -26,7 +29,7 @@ def format(model_type):
         algal_data.drop(columns=["variable"], inplace=True)
 
         # remove year, only keep month and day
-        algal_data["datetime"] = algal_data["datetime"].str[5:10]
+        # algal_data["datetime"] = algal_data["datetime"].str[5:10]
 
         # get index for split between fcre and bvre
         for ind, value in enumerate(algal_data['site_id']):
@@ -42,6 +45,15 @@ def format(model_type):
         bvre_chla_data.to_csv(path_or_buf="./data/bvre_data" + datestring + ".csv", index=False)
         fcre_chla_data.to_csv(path_or_buf="./data/fcre_data" + datestring + ".csv", index=False)
 
+        if (reservoir=="fcre"):
+            return fcre_chla_data
+        
+        elif (reservoir=="bvre"):
+            return bvre_chla_data
+        
+        else:
+            raise Exception("Invalid reservoir input!")
+
 
 if __name__ == "__main__":
-    format("null")
+    format("null", "fcre")
