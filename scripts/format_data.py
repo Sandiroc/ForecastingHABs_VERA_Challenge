0d845cf65fddbba7e3c92c91fa6a411a839e5c1e
Sandiroc <bsandilya1908@gmail.com>
2024-04-15 00:27:22 -0400

    split on reservoir

--- scripts/format_data.py
+++ scripts/format_data.py
@@ -3,9 +3,45 @@ import pandas as pd
 import read_data
 
 # get the data
-datestring = datetime.now().strftime("%Y-%m-%d")
-file_name_root = "VERA_data" + datestring
 read_data.read()
 
-# separate into target vars
-TARGET_VAR_NAMES = list("Temp_C_mean")
+def format(model_type):
+    """Format the data obtained from the VERA website, parameter included depending on what features are included in the lstm"""
+
+    # get date
+    date = datetime.now()
+    datestring = date.strftime("%Y-%m-%d")
+        
+    # filepath, get csv
+    algal_data = pd.read_csv("raw_data/VERA_data_" + datestring + ".csv")
+    
+    if (model_type == "null"):
+
+        # drop empty observations
+        algal_data.dropna(subset=['observation'], inplace = True)
+
+        # get only chloropyhll-a
+        algal_data = algal_data[algal_data['variable'] == "Chla_ugL_mean"]
+        algal_data.rename(columns={"observation" : "Chla_ugl_mean"}, inplace=True)
+        algal_data.drop(columns=["variable"], inplace=True)
+
+        # remove year, only keep month and day
+        algal_data["datetime"] = algal_data["datetime"].str[5:10]
+
+        # get index for split between fcre and bvre
+        for ind, value in enumerate(algal_data['site_id']):
+            if value =='bvre' and algal_data['site_id'].iloc[ind - 1] == 'fcre':
+                split_index = ind
+                break
+
+        bvre_chla_data = algal_data[split_index:]
+        fcre_chla_data = algal_data[:split_index]
+        
+        # send to CSVs
+        algal_data.to_csv(path_or_buf="./data/formatted_" + datestring + ".csv", index=False)
+        bvre_chla_data.to_csv(path_or_buf="./data/bvre_data" + datestring + ".csv", index=False)
+        fcre_chla_data.to_csv(path_or_buf="./data/fcre_data" + datestring + ".csv", index=False)
+
+
+if __name__ == "__main__":
+    format("null")
--- scripts/read_data.py
+++ scripts/read_data.py
@@ -2,13 +2,15 @@ from datetime import datetime
 import pandas as pd
 
 def read():
+    """Read data in from the VERA website"""
+    
     # localize data directly from VERA site
     url = "https://renc.osn.xsede.org/bio230121-bucket01/vera4cast/targets/project_id=vera4cast/duration=P1D/daily-insitu-targets.csv.gz"
     data = pd.read_csv(url)
 
     date = datetime.now()
     datestring = date.strftime("%Y-%m-%d")
-    data.to_csv("./data/VERA_data_" + datestring + ".csv", index=False)
+    data.to_csv("./raw_data/VERA_data_" + datestring + ".csv", index=False)
 
 
 
