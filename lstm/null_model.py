41ae8e83844381772325e8ca182e3a3e6ac414d9
Sandiroc <bsandilya1908@gmail.com>
2024-05-05 06:49:00 -0400

    finished forecast and submission capability!

--- lstm/null_model.py
+++ lstm/null_model.py
@@ -93,6 +93,7 @@ def noise_val(X_test, y_test, model: Sequential, noises=(0.01, 0.05, 0.1, 0.15, 
 
 
 def epoch_eval(X_test, y_test, model: Sequential, epochs=range(5, 50, 5)):
+    """Find the optimal number of epochs"""
         
     rmse = list()
 
@@ -117,6 +118,7 @@ def epoch_eval(X_test, y_test, model: Sequential, epochs=range(5, 50, 5)):
     return rmse, epochs
 
 def batch_eval(X_test, y_test, model: Sequential, batches=(2, 4, 8, 16, 32)):
+    """Find the optimal number of batches"""
         
     rmse = list()
 
@@ -141,7 +143,17 @@ def batch_eval(X_test, y_test, model: Sequential, batches=(2, 4, 8, 16, 32)):
     return rmse, batches
 
 
+def fit_model(model, X_train, y_train, epochs=30, batches=16):
+    """ Wrapper function """
+    model.fit(X_train, y_train, epochs=30, batch_size=16, verbose=1)
+
+    return model
+
+"""
 def seq_eval(X_test, y_test, model: Sequential, sequences=range(1,101)):
+    # Find the optimal number of sequences 
+    # inconclusive, number of sequences is variable.
+    # standard sequence length is within +5 range of forecast window
         
     rmse = list()
 
@@ -163,68 +175,9 @@ def seq_eval(X_test, y_test, model: Sequential, sequences=range(1,101)):
         rmse.append(np.sqrt(mean_squared_error(y_test_inverse, y_pred_inverse)))
 
     
-    return rmse, sequences
+    return rmse, sequences """
 
-
-
-# ------------------------------------------------------------------------------------------------
-
-# get fcre data
-data, reservoir = get_data(reservoir="fcre")
-
-# normalize chl-a observations
-data, scaler = normalize_and_format(data)
-
-# Choose sequence length
-# look 8 days back, forecast 7 days into the future
-sequence_length = 8
-forecast_horizon = 7
-
-# Create sequences
-X, y = create_sequences(data['Chla_ugL_mean'], sequence_length, forecast_horizon)
-
-# Split the data into training and testing sets
-
-train_size = int(len(X) * 0.7)
-X_train, X_test = X[:train_size], X[train_size:]
-y_train, y_test = y[:train_size], y[train_size:]
-
-# Reshape the input data for LSTM
-X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-
-# define and train
-model = define_model(sequence_length, forecast_horizon)
-model.fit(X_train, y_train, epochs=30, batch_size=16, verbose=1)
-
-# Make predictions
-y_pred = model.predict(X_test)
-
-# Inverse transform the predictions
-y_pred_inverse = scaler.inverse_transform(y_pred)
-y_test_inverse = scaler.inverse_transform(y_test.reshape(-1,forecast_horizon))
-
-# Print the predicted chlorophyll-a value for tomorrow
-print("Predicted chlorophyll-a value for " + str(forecast_horizon) + " days into the future:", y_pred_inverse[-1])
-
-# save y_test and y_hat
-np.savetxt("./predicted.txt", y_pred_inverse)
-np.savetxt("./actual.txt", y_test_inverse)
-np.savetxt("./xtrain.txt", scaler.inverse_transform(X_test.reshape(-1,1)))
-
-# rmse
-print("Model RMSE: ", np.sqrt(mean_squared_error(y_test_inverse, y_pred_inverse)))
-
-# plot
-plt.figure(figsize=(10, 6))
-plt.plot(y_test_inverse, label='Actual')
-plt.plot(y_pred_inverse, label='Predicted')
-plt.xlabel('Time')
-plt.ylabel('Mean Chlorophyll-a content (ug/L)')
-plt.title('Actual vs Predicted Chlorophyll-a Values at ' + reservoir)
-plt.legend()
-plt.savefig("./plots/null_preds_" + reservoir + ".png")
-
+# parameter tuning ----------------------------------------
 """
 # test noise
 noise_rmse, noises = noise_val(X_test, y_test, model)
@@ -273,3 +226,74 @@ plt.ylabel('RMSE')
 plt.title("RMSE vs. Number of Sequences")
 plt.savefig("./plots/null_sequences.png")
 """
+
+# Forecast ------------------------------------------------
+
+def forecast(reservoir: str, forecast_horizon):
+    # get fcre data
+    data, reservoir = get_data(reservoir=reservoir)
+
+    # normalize chl-a observations
+    data, scaler = normalize_and_format(data)
+
+    # Choose sequence length
+    # look 8 days back, forecast 7 days into the future
+    sequence_length = forecast_horizon + 1
+
+    # Create sequences
+    X, y = create_sequences(data['Chla_ugL_mean'], sequence_length, forecast_horizon)
+
+    # Split the data into training and testing sets
+
+    train_size = int(len(X) * 0.7)
+    X_train, X_test = X[:train_size], X[train_size:]
+    y_train, y_test = y[:train_size], y[train_size:]
+
+    # Reshape the input data for LSTM
+    X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+    X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+    # define and train
+    model = define_model(sequence_length, forecast_horizon)
+    model.fit(X_train, y_train, epochs=30, batch_size=16, verbose=0)
+
+    # Make predictions
+    y_pred = model.predict(X_test)
+
+    # Inverse transform the predictions
+    y_pred_inverse = scaler.inverse_transform(y_pred)
+    y_test_inverse = scaler.inverse_transform(y_test.reshape(-1,forecast_horizon))
+
+    # Print the predicted chlorophyll-a value for tomorrow
+    #print("Predicted chlorophyll-a value for " + 
+    #    str(forecast_horizon) + " days into the future:", y_pred_inverse[-1])
+
+    # save y_test and y_hat
+    np.savetxt("./predicted.txt", y_pred_inverse)
+    np.savetxt("./actual.txt", y_test_inverse)
+    np.savetxt("./xtrain.txt", scaler.inverse_transform(X_test.reshape(-1,1)))
+
+    # rmse
+    #print("Model RMSE: ", np.sqrt(mean_squared_error(y_test_inverse, y_pred_inverse)))
+    
+    # get the latest forecasts
+    curr_forecast = y_pred_inverse[-1]
+    #print(curr_forecast)
+
+    # plot
+    plt.figure(figsize=(10, 6))
+    plt.plot(y_test_inverse, label='Actual')
+    plt.plot(y_pred_inverse, label='Predicted')
+    plt.xlabel('Time')
+    plt.ylabel('Mean Chlorophyll-a content (ug/L)')
+    plt.title('Actual vs Predicted Chlorophyll-a Values at ' + reservoir)
+    plt.legend()
+    plt.savefig("./plots/null_preds_" + reservoir + ".png")
+
+    return curr_forecast
+
+
+
+
+if __name__ == "__main__":
+    forecast("fcre")
---
+++ lstm/run_model.py
@@ -0,0 +1,99 @@
+import numpy as np
+import sys
+from datetime import datetime, timedelta
+import pandas as pd
+import time
+import os
+import null_model as nm
+
+NUM_ITER = 1
+
+def binary_conversion(array: np.ndarray):
+    """ If chlorophyll-a amount is greater than 20, then an algal bloom will occur """
+    binaries = np.zeros_like(array)
+    binaries[array > 20] = 1
+    return binaries
+
+
+def generate_predictions(reservoir: str, forecast_len):
+    """ Generate the bloom binary mean """
+    #start = time.time()
+    forecasts = list()
+
+    for i in range(NUM_ITER):
+        curr_forecast = nm.forecast(reservoir, forecast_len)
+        forecasts.append(binary_conversion(curr_forecast))
+
+
+    stacked = np.stack(forecasts, axis=0)
+    means = np.mean(stacked, axis=0)
+    end = time.time()
+    #print(end - start)
+
+    return means
+
+
+def generate_dates(forecast_len):
+    """ Generate dates of forecast """
+    today = datetime.today() + timedelta(1)
+    ref_dates = list()
+
+    for i in range(forecast_len):
+        ref_dates.append(today.strftime("%Y-%m-%d"))
+        today += timedelta(1)
+
+    return ref_dates
+
+
+def generate_csv(reservoir: str, forecast_len):
+    """ Generate CSV with forecast info for submission """
+    model_id = ["protist" for _ in range(forecast_len)]
+    dates = generate_dates(forecast_len)
+    today = [datetime.today().strftime("%Y-%m-%d") for _ in range(forecast_len)]
+    site_id = [reservoir for _ in range(forecast_len)]
+    variable = ["Bloom_binary_mean" for _ in range(forecast_len)]
+    family = ["bernoulli" for _ in range(forecast_len)]
+    parameter = ["prob" for _ in range(forecast_len)]
+    prediction = list(generate_predictions(reservoir, forecast_len))
+
+    if reservoir == "fcre":
+        depth = [1.6 for _ in range(forecast_len)]
+    else:
+        depth = [1.5 for _ in range(forecast_len)]
+
+    project_id = ["vera4cast" for _ in range(forecast_len)]
+    duration = ["P1D" for _ in range(forecast_len)]
+
+    submission = {
+        'model_id': model_id,
+        'datetime': dates,
+        'reference_datetime': today,
+        'site_id': site_id,
+        'variable': variable,
+        'family': family,
+        'parameter': parameter,
+        'prediction': prediction,
+        'depth_m': depth,
+        'project_id': project_id,
+        'duration': duration
+    }
+
+    # remove submissions past the last 10 days
+    directory = "./submissions"
+    files = os.listdir(directory)
+    file_count = len(files)
+
+    if file_count > 10:
+        for file in files:
+            file_path = os.path.join(directory, file)
+            os.remove(file_path)
+
+    # send to csv
+    pd.DataFrame(submission).to_csv("./submissions/vera_submission_" + datetime.today().strftime("%Y-%m-%d") + ".csv")
+
+
+
+
+
+if __name__ == "__main__":
+    generate_csv(sys.argv[1], 7)
